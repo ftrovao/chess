2e4c68a2beaf3f59f39380a075b17db2f7363e57
Fernando <trovaocanada@gmail.com>
2023-04-22 21:27:20 -0500

    Possibles moves of a piece

--- chessSystem/src/chess/pieces/King.java
+++ chessSystem/src/chess/pieces/King.java
@@ -16,4 +16,12 @@ public class King extends ChessPiece{
 		return "K";
 	}
 
+	@Override
+	public boolean[][] possibleMoves() {
+		//method getBoard() was create in Piece in order to access Board board. This is why
+		//there's a relation between Piece and Board. Piece has access to Board board.
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+
 }
